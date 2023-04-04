aae760c8190c9bf29985af592fc73202af9cc79d
luciadesperati <126161614+luciadesperati@users.noreply.github.com>
2023-04-04 09:39:47 +0100

    API round 1

--- src/index.js
+++ src/index.js
@@ -47,7 +47,7 @@ let minutes = now.getMinutes().toString().padStart(2, "0");
 dateDisplay.innerHTML = `${day} ${hour}:${minutes}`;
 let weatherDescription = document.querySelector("#weather-description");
 
-function displayForecast() {
+function displayForecast(daysFromApi) {
   let forecastElement = document.querySelector("#weather-forecast");
   let forecastHTML = `<hr />
                   <div
@@ -55,22 +55,66 @@ function displayForecast() {
                   >
                   <div class="col-1"></div>`;
 
-  let days = ["Mon", "Tue", "Wed", "Thu", "Fri"];
-  days.forEach(function (day) {
+  daysFromApi.forEach(function (currentDay) {
+    // Day names forecast -> depending on date
+    let weekDays = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+    console.log(weekDays);
+    let forecastDays = "";
+    // based on the current day > show 5 elements > possibly with the forEach?
+
+    console.log(forecastDays);
+    // Dates forecast > depending on date
+    let forecastDates = new Date(currentDay.time * 1000);
+    console.log(forecastDates);
+    let startDay = weekDays[forecastDates.getDay()];
+    console.log(startDay);
+
+    let months = [
+      "Jan",
+      "Feb",
+      "Mar",
+      "Apr",
+      "May",
+      "Jun",
+      "Jul",
+      "Aug",
+      "Sep",
+      "Oct",
+      "Nov",
+      "Dec",
+    ];
+    let date = forecastDates.getDate();
+    console.log(date);
+
+    let month = months[forecastDates.getMonth()];
+    console.log(month);
+
+    // Icons > depending on days
+    let forecastWeatherIcon = currentDay.condition.icon_url;
+    console.log(forecastWeatherIcon);
+
+    // Temperature Max
+    let forecastTempMax = Math.round(currentDay.temperature.maximum);
+    console.log(forecastTempMax);
+
+    // Temperature Min
+    let forecastTempMin = Math.round(currentDay.temperature.minimum);
+    console.log(forecastTempMin);
+
     forecastHTML =
       forecastHTML +
       `
   <div class="col-2">
-                      <p class="weekday-list mb-0">${day}</p>
-                      <p class="day-list">6 Mar</p>
+                      <p class="weekday-list mb-0">${startDay}</p>
+                      <p class="day-list">${date} ${month}</p>
                       <img
                         class="emoji-list"
                         id="weather-icon-list"
-                        src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/clear-sky-day.png"
+                        src="${forecastWeatherIcon}"
                       />
                       <span class="d-flex justify-content-center">
-                        <p class="temperature-max">23</p>
-                        <p class="temperature-min">23</p>
+                      <p class="temperature-min">${forecastTempMin}</p>
+                      <p class="temperature-max">${forecastTempMax}</p>
                       </span>
                     </div>
   `;
@@ -88,8 +132,6 @@ function onSearchCity(event) {
   let apiKey = "2b6be0a88f02eco343b0c579f343cbt9";
   let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${searchedCity}&key=${apiKey}&units=metric`;
   console.log(apiUrl);
-
-  displayForecast();
 
   function showWeather(data) {
     console.log(data);
@@ -111,6 +153,10 @@ function onSearchCity(event) {
     wind.innerHTML = `${currentWind}`;
 
     currentTempCel = currentTemp;
+
+    let daysFromApi = data.data.daily;
+    daysFromApi = daysFromApi.slice(0, 5);
+    displayForecast(daysFromApi);
   }
 
   axios.get(apiUrl).then(showWeather);
