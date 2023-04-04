a5b68cba43eee3ec320d300153efed5095ef2426
luciadesperati <126161614+luciadesperati@users.noreply.github.com>
2023-04-04 07:54:34 +0100

    JS round 1

--- src/index.js
+++ src/index.js
@@ -47,6 +47,39 @@ let minutes = now.getMinutes().toString().padStart(2, "0");
 dateDisplay.innerHTML = `${day} ${hour}:${minutes}`;
 let weatherDescription = document.querySelector("#weather-description");
 
+function displayForecast() {
+  let forecastElement = document.querySelector("#weather-forecast");
+  let forecastHTML = `<hr />
+                  <div
+                    class="pt-4 mt-3 row forecast d-flex"
+                  >
+                  <div class="col-1"></div>`;
+
+  let days = ["Mon", "Tue", "Wed", "Thu", "Fri"];
+  days.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      `
+  <div class="col-2">
+                      <p class="weekday-list mb-0">${day}</p>
+                      <p class="day-list">6 Mar</p>
+                      <img
+                        class="emoji-list"
+                        id="weather-icon-list"
+                        src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/clear-sky-day.png"
+                      />
+                      <span class="d-flex justify-content-center">
+                        <p class="temperature-max">23</p>
+                        <p class="temperature-min">23</p>
+                      </span>
+                    </div>
+  `;
+  });
+
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+}
+
 // when search > show temp of searched city
 function onSearchCity(event) {
   event.preventDefault();
@@ -55,6 +88,8 @@ function onSearchCity(event) {
   let apiKey = "2b6be0a88f02eco343b0c579f343cbt9";
   let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${searchedCity}&key=${apiKey}&units=metric`;
   console.log(apiUrl);
+
+  displayForecast();
 
   function showWeather(data) {
     console.log(data);
